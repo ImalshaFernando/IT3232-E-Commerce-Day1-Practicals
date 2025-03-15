e561548d378b60b832071fed2e706a6b04af954e
Imalsha Fernando <mni.fernando326@gmail.com>
2025-03-15 20:18:17 +0530

    Question 04

---
+++ Ex4.java
@@ -0,0 +1,28 @@
+//4)print the odd numbers between 1 to 20
+
+class Ex4{
+public static void main(String[] args){
+    for(int i=1; i<20; i++){
+        if(i%2 == 1)
+        {
+            System.out.println(i);
+        }
+        }
+    }
+  
+}
+
+/*
+D:\E-Commerce>javac Ex4.java
+
+D:\E-Commerce>java Ex4
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
