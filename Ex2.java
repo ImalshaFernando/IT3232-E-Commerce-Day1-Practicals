d06fe2b3fa807a4b49ea3bbb6bf5c7d05d37c255
Imalsha Fernando <mni.fernando326@gmail.com>
2025-03-15 20:11:05 +0530

    Auestion 02

---
+++ Ex2.java
@@ -0,0 +1,26 @@
+//2)print numbers from 1 to 10
+
+class Ex2{
+public static void main(String[] args){
+    for(int i=1; i<11; i++){
+          System.out.println(i);
+        }
+    }
+  
+}
+
+/*
+D:\E-Commerce>javac Ex2.java
+
+D:\E-Commerce>java Ex2
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
