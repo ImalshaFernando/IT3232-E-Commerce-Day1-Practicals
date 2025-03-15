9f9d21bfe42f55526220a78a2e3e95e2e66039d8
Imalsha Fernando <mni.fernando326@gmail.com>
2025-03-15 20:05:38 +0530

    Question 01

---
+++ Ex1.java
@@ -0,0 +1,14 @@
+//Print a message 'Hello World'
+
+class Ex1{
+public static void main(String[] args){
+    System.out.print("Hello World");
+}
+}
+
+/*
+D:\E-Commerce>javac Ex1.java
+
+D:\E-Commerce>java Ex1
+Hello World
+*/
