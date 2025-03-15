d9d1a1831d6be020d55dc8fd7ab9a8df7f1a0305
Imalsha Fernando <mni.fernando326@gmail.com>
2025-03-15 20:14:59 +0530

    Question 03

---
+++ Ex3.java
@@ -0,0 +1,24 @@
+//3)print the numbers in reverse
+
+class Ex3{
+public static void main(String[] args){
+    for(int i=10; i>0; i--){
+          System.out.println(i);
+        }
+    }
+  
+}
+
+/*
+D:\E-Commerce>javac Ex3.java
+D:\E-Commerce>java Ex3
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
